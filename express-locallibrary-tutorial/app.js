59ba2e6e455d4fd0396d91c6f05da05f0810ab4e
Bobby <48532176+robertcs3@users.noreply.github.com>
2022-11-21 16:00:52 -0600

    pug

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -6,13 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const catalogRouter = require("./routes/catalog"); //Import routes for "catalog"
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
